44809524494f9e574e8905494f1d6320d8e445fe
ovezthaking <oliwerx12@gmail.com>
2025-10-29 23:16:46 +0100

    GET and POST

--- HTTP Methods/Mood Tracker (CRUD Node.js app)/app.js
+++ HTTP Methods/Mood Tracker (CRUD Node.js app)/app.js
@@ -3,16 +3,36 @@ const http = require('http');
 let mood = '';
 
 const server = http.createServer((request, response) => {
-    
-    if(request.url == '/'){
-        response.writeHead(200, {'Content-type' : "text/plain; charset=utf8"});
-        response.write(mood);
-
         if(request.method === "GET"){
-            console.log(mood);
+            if(!mood){
+                console.log("You haven\'t posted any mood yet");
+                response.writeHead(200, {'Content-type' : "text/plain; charset=utf8"});
+                response.end("No mood...");
+            }
+            else{
+                console.log(mood);
+                response.writeHead(200, {'Content-type' : "text/plain; charset=utf8"});
+                response.end(mood);
+            }
         }
         else if(request.method === "POST"){
-            
+            if(!mood){
+                let newMood = '';
+                request.on('data', (chunk) => {
+                    newMood += chunk;
+                });
+                
+                request.on('end', () => {
+                    mood = newMood;
+                    console.log(`Your current mood: ${mood}`);
+                });
+                response.writeHead(200, {'Content-type' : "text/plain; charset=utf8"});
+                response.end(mood); 
+            }
+            else{
+                response.writeHead(200, {'Content-type' : "text/plain; charset=utf8"});
+                response.end(`Here exist your past mood: ${mood}`); 
+            }
         }
         else if(request.method === "PUT"){
             
@@ -27,13 +47,28 @@ const server = http.createServer((request, response) => {
             response.writeHead(404, {'Content-type' : "text/plain; charset=utf8"});
             response.end("404 Not Found");
         }
-    }
-    else{
-        response.writeHead(404, {'Content-type' : "text/plain; charset=utf8"});
-        response.end("404 Not Found");
-    }
+    
 });
 
 server.listen(3000, () => {
     console.log("Server is running at http://localhost:3000");
 });
+
+
+
+// example cURL commands:
+
+// POST a new mood:
+// curl -X POST http://localhost:3000 -d "Feeling great 💅"
+
+// GET the mood:
+// curl http://localhost:3000
+
+// PUT a mood instead:
+// curl -X PUT http://localhost:3000 -d "Feeling not great "
+
+// PATCH the mood:
+// curl -X PATCH http://localhost:3000 -d "Feeling sad 😢"
+
+// DELETE the mood:
+// curl -X DELETE http://localhost:3000
