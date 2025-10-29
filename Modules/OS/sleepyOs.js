0340c4f54ecc437d62607607892a770bb8da6235
ovezthaking <oliwerx12@gmail.com>
2025-10-29 03:02:15 +0100

    os

---
+++ Modules/OS/sleepyOs.js
@@ -0,0 +1,19 @@
+// 7 days in seconds 
+const SECONDS_IN_A_WEEK = 60 * 60 * 24 * 7;
+// Convert uptime to hours, minutes, and seconds
+const os = require('os');
+
+const uptimeInSeconds = os.uptime();
+
+const hours = Math.floor(uptimeInSeconds / 3600);
+const minutes = Math.floor((uptimeInSeconds % 3600) / 60);
+const seconds = Math.floor(uptimeInSeconds % 60);
+
+console.log(`You are awake for ${hours} hours ${minutes} minutes ${seconds} seconds`);
+
+if (os.uptime()<SECONDS_IN_A_WEEK){
+    console.log('You are not so tired');
+}
+else if (os.uptime()>SECONDS_IN_A_WEEK){
+    console.log('Do u sleep sometimes?');
+}
