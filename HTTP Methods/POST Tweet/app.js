0081ed10682ca75bb0c6e5482f598d71c3f1d17e
ovezthaking <oliwerx12@gmail.com>
2025-10-29 22:06:15 +0100

    POST

---
+++ HTTP Methods/POST Tweet/app.js
@@ -0,0 +1,26 @@
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+    let message = '';
+    if (request.method === 'POST'){
+        request.on('data', (chunk) => {
+            message += chunk;
+        });
+
+        request.on('end', () => {
+            console.log('New message: ', message);
+
+            response.writeHead('200', {'Content-type' : 'text/plain'});
+            response.end("Message received " + message);
+        });
+    }
+    else{
+        response.writeHead(404, {'Content-type' : "text/plain"});
+        response.end('Back to ur terminal');
+    }
+
+});
+
+server.listen(3000, () => {
+    console.log('http://localhost:3000/');
+});
